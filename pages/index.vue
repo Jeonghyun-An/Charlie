38a72cbf4a7ee0d89258202dbad0e6ae79ad2523
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2025-02-18 16:22:02 +0900

    config: primevue theme setting

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <DataTable :value="products" tableStyle="min-width: 50rem">
+      <Column field="code" header="Code"></Column>
+      <Column field="name" header="Name"></Column>
+      <Column field="category" header="Category"></Column>
+      <Column field="quantity" header="Quantity"></Column>
+    </DataTable>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+
+const products = ref([
+  { code: 1004, name: "정민석", category: "ace", quantity: 200 },
+  { code: 1004, name: "정민석", category: "ace", quantity: 200 },
+  { code: 1004, name: "정민석", category: "ace", quantity: 200 },
+  { code: 1004, name: "정민석", category: "ace", quantity: 200 },
+  { code: 1004, name: "정민석", category: "ace", quantity: 200 },
+]);
+</script>
