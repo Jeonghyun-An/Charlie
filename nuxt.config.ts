f0727d36e74f20615dae23420a70d9bd7d316e13
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2025-02-18 15:43:40 +0900

    config: add vue-use nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/icon", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
+  modules: [
+    "@nuxt/icon",
+    "@pinia/nuxt",
+    "pinia-plugin-persistedstate/nuxt",
+    "@vueuse/nuxt",
+  ],
 });
