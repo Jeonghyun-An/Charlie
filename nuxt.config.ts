d7739e52770b45d8a4766566ea299157f2fecb8f
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2025-02-18 15:43:11 +0900

    config: add nuxt pinia module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/icon"],
+  modules: ["@nuxt/icon", "@pinia/nuxt", "pinia-plugin-persistedstate/nuxt"],
 });
