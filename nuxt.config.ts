f9b62391ab99f0efa7574a38c8754673d12ed73d
Minseok Jung <37059223+Minseok0917@users.noreply.github.com>
2025-02-18 16:51:33 +0900

    config: cross-env, dotenv를 사용한 개발환경 환경변수 처리

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 import Aura from "@primevue/themes/aura";
+import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+
+(() => {
+  const mode = process.env.MODE ?? "development";
+  const environmentPath = path.resolve(process.cwd(), `envs/.env.${mode}`);
+  const existEnvironment = fs.existsSync(environmentPath);
+
+  if (existEnvironment) {
+    dotenv.config({ path: environmentPath });
+    console.log(`Loaded environment variables from ${environmentPath}`);
+  } else {
+    console.warn(`.env 파일이 ${environmentPath}에 존재하지 않습니다.`);
+  }
+})();
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
