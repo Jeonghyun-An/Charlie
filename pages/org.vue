b34502630422874a3f83dd2e5eb4627df2f173b0
Hyonii <131145818+Jeonghyun-An@users.noreply.github.com>
2025-03-21 18:01:06 +0900

    [feat] 내 프로필 컴포넌트 생성 및 연결

--- pages/org.vue
+++ pages/org.vue
@@ -1,26 +1,30 @@
 <template>
-    <div class="max-w-2xl mx-auto p-4">
-        <h1 class="text-xl font-semibold mb-4">조직도</h1>
-        <Accordion :multiple="true" class="border-none shadow-none">
-            <AccordionTab header="내 프로필">
-                <ul class="ml-4 p-2 list-disc">
-                    <li v-for="(member, index) in myProfile" :key="index">
-                        {{ member }}
-                    </li>
-                </ul>
-            </AccordionTab>
-            <AccordionTab header="지뉴소프트(주)" :expanded="true">
-                <OrgAccordion :data="orgData" />
-            </AccordionTab>
-        </Accordion>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-6 p-6">
+        <div class="max-w-2xl mx-auto p-4">
+            <h1 class="text-xl font-semibold mb-4">조직도</h1>
+            <Accordion :multiple="true" class="border-none shadow-none">
+                <AccordionTab header="내 프로필">
+                    <OrgMine :data="myData" />
+                </AccordionTab>
+                <AccordionTab header="지뉴소프트(주)" :expanded="true">
+                    <OrgAccordion :data="orgData" />
+                </AccordionTab>
+            </Accordion>
+        </div>
+        <!-- 드롭 대상 -->
+        <div>
+            <DropTarget />
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
-import OrgAccordion from "@/components/OrgAccordion.vue";
+import OrgMine from "@/components/OrgMine.vue";
 import orgChartData from "@/assets/data/orgChart.json";
+import OrgAccordion from "@/components/OrgAccordion.vue";
+import orgMyData from "@/assets/data/myData.json";
+import DropTarget from "@/components/DropTarget.vue";
 
-const myProfile = ref(["안정현 매니저"]);
-
+const myData = orgMyData;
 const orgData = orgChartData;
 </script>
